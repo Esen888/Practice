1be9a34889d26a44cbfde7005922f60828e6fd92
Esen888 <esensharsheev808@gmail.com>
2022-10-07 20:23:37 +0600

    first commit

---
+++ lesson2.dart
@@ -0,0 +1,58 @@
+void main(){
+  num aa = 6;
+  num bb = 6.6;
+int a = 5;
+int b = 15;
+int c = 20;
+int d = 25;
+int e = 30;
+double f = 5.1;
+double g = 5.2;
+double h = 5.3;
+double j = 5.4;
+double k = 5.5;
+String mgs = 'major';
+String msg1 = 'minor';
+String msg2 = 'sonor';
+List calc = [1, 2, 3, 4, 5];
+Map studentForm ={'name' : 'Esen', 'education' : 'High', 'age': 27};
+final gg;
+gg = 9.8;
+const p =3.14;
+dynamic ggg = 9.8;
+ggg = 10;
+var gggg = 'qwerty';
+gggg = "qwery";
+
+var heart_symbol = '❤️';
+
+var laugh_symbol = '😂';
+
+print(aa);
+print(bb);
+
+print(a);
+print(b);
+print(c);
+print(d);
+print(e);
+print(f);
+print(g);
+print(h);
+print(j);
+print(k);
+print(p);
+
+print(mgs);
+print(msg1);
+print(msg2);
+
+print(calc);
+print(studentForm);
+print(gg);
+print(ggg);
+print(gggg);
+
+print(heart_symbol);
+print(laugh_symbol);
+}
